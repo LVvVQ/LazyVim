5127bc334f96fc304ed21eea452ca80cc3318605
LVvVQ <287535753@qq.com>
2024-11-02 14:52:12 +0800

    Fix: change ruff "K" keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,12 +10,15 @@ return {
       keys[#keys + 1] = { "gh", vim.lsp.buf.hover }
       keys[#keys + 1] = { "gH", vim.lsp.buf.signature_help }
 
+      opts.setup = opts.setup or {}
       opts.setup.eslint = function()
         require("lazyvim.util").lsp.on_attach(function(client)
           if client.name == "eslint" then
             client.server_capabilities.documentFormattingProvider = true
           elseif client.name == "tsserver" then
             client.server_capabilities.documentFormattingProvider = false
+          elseif client.name == "ruff" then
+            vim.keymap.set("n", "K", "5k", { buffer = 0 })
           end
         end)
       end
