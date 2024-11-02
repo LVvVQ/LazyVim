30440ccd686388822377cdc56400a4c25d062e4f
LVvVQ <287535753@qq.com>
2024-11-02 14:53:07 +0800

    Enable lazyvim.plugins.extras.coding.luasnip

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     { import = "lazyvim.plugins.extras.editor.telescope" },
+    { import = "lazyvim.plugins.extras.coding.luasnip" },
 
     -- import/override with your plugins
     { import = "plugins" },
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,11 +3,7 @@ return {
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
     local cmp = require("cmp")
-
-    opts.experimental = {
-      ghost_text = false,
-      native_menu = false,
-    }
+    local luasnip = require("luasnip")
 
     opts.mapping = cmp.mapping.preset.insert({
       ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
@@ -17,7 +13,39 @@ return {
       ["<C-u>"] = cmp.mapping.scroll_docs(-4),
       ["<C-d>"] = cmp.mapping.scroll_docs(4),
       ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      ["<CR>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          if luasnip.expandable() then
+            luasnip.expand()
+          else
+            cmp.confirm({
+              select = true,
+            })
+          end
+        else
+          fallback()
+        end
+      end),
+
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.locally_jumpable(1) then
+          luasnip.jump(1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.locally_jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
       ["<S-CR>"] = cmp.mapping(function(fallback)
         cmp.abort()
         fallback()
