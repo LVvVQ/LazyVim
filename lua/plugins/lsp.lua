c29bd04e7eadc458a6ae13da54a41c0525fb1eaa
LVvVQ <287535753@qq.com>
2024-11-02 13:49:43 +0800

    Fix: go.nvim plugin configuration in the wrong location

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,35 +1,26 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- disable a keymap
-    keys[#keys + 1] = { "K", false }
-    keys[#keys + 1] = { mode = "i", "<C-k>", false }
-    -- change a keymap
-    keys[#keys + 1] = { "gh", vim.lsp.buf.hover }
-    keys[#keys + 1] = { "gH", vim.lsp.buf.signature_help }
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- disable a keymap
+      keys[#keys + 1] = { "K", false }
+      keys[#keys + 1] = { mode = "i", "<C-k>", false }
+      -- change a keymap
+      keys[#keys + 1] = { "gh", vim.lsp.buf.hover }
+      keys[#keys + 1] = { "gH", vim.lsp.buf.signature_help }
 
-    return {
-      servers = {
-        jdtls = {
-          keys = {
-            { "<leader>oi", "<Cmd>lua require('jdtls').organize_imports()<CR>", desc = "Organize Imports" },
-          },
-        },
-      },
-      setup = {
-        eslint = function()
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
-      },
-    }
-  end,
+      opts.setup.eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end
+    end,
+  },
   {
     "ray-x/go.nvim",
     dependencies = {
