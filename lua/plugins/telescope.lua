66317dafcf88a48b8e327455c34cecf704ab30eb
LVvVQ <287535753@qq.com>
2024-11-03 04:13:31 +0800

    Add Telescope's ignore files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
 
     opts.defaults = opts.defaults or {}
     opts.defaults.path_display = { "smart" }
-    opts.defaults.file_ignore_patterns = { "node_modules", ".git", "target", ".DS_Store", "__pycache__" }
+    opts.defaults.file_ignore_patterns = { "node_modules", ".git", "target", ".DS_Store", "__pycache__", ".venv" }
     opts.defaults.mappings = opts.defaults.mappings or {}
     opts.defaults.mappings.i = vim.tbl_extend("force", opts.defaults.mappings.i or {}, {
       ["<C-j>"] = actions.move_selection_next,
