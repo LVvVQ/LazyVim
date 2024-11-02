4b8c3e4e75db5c60bc2c2b0b56dfdc4f91a50dc1
LVvVQ <287535753@qq.com>
2024-11-03 04:12:44 +0800

    Fix: Fix lsp-config plugin configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,18 +10,19 @@ return {
       keys[#keys + 1] = { "gh", vim.lsp.buf.hover }
       keys[#keys + 1] = { "gH", vim.lsp.buf.signature_help }
 
-      opts.setup = opts.setup or {}
-      opts.setup.eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" then
-            client.server_capabilities.documentFormattingProvider = false
-          elseif client.name == "ruff" then
-            vim.keymap.set("n", "K", "5k", { buffer = 0 })
-          end
-        end)
-      end
+      opts.setup = vim.tbl_deep_extend("force", opts.setup or {}, {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            elseif client.name == "ruff" then
+              vim.keymap.set("n", "K", "5k", { buffer = 0 })
+            end
+          end)
+        end,
+      })
     end,
   },
   {
