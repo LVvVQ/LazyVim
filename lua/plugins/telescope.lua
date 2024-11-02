8af3d4c101903f75e49c33c3d3e7c89420f36cec
LVvVQ <287535753@qq.com>
2024-11-02 13:56:12 +0800

    Fix: telescope wrong configuration
    Add lazyvim.plugins.extras.editor.telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,15 @@
 return {
   "nvim-telescope/telescope.nvim",
-  opts = function()
+  opts = function(_, opts)
     local actions = require("telescope.actions")
-    return {
-      defaults = {
-        mappings = {
-          i = {
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-k>"] = actions.move_selection_previous,
-          },
-        },
-      },
-    }
+
+    opts.defaults = opts.defaults or {}
+    opts.defaults.path_display = { "smart" }
+    opts.defaults.file_ignore_patterns = { "node_modules", ".git", "target", ".DS_Store", "__pycache__" }
+    opts.defaults.mappings = opts.defaults.mappings or {}
+    opts.defaults.mappings.i = vim.tbl_extend("force", opts.defaults.mappings.i or {}, {
+      ["<C-j>"] = actions.move_selection_next,
+      ["<C-k>"] = actions.move_selection_previous,
+    })
   end,
 }
